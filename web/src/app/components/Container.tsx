cd30d79bb93557c4f5c30eb1b9961251883febf4
wooseok <youkn0wthat@naver.com>
2025-07-06 19:40:32 +0900

    feat: add responsive Container layout component

---
+++ web/src/app/components/Container.tsx
@@ -0,0 +1,7 @@
+export default function Container({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="px-6 md:px-10 lg:px-[120px] max-w-screen-xl mx-auto w-full">
+      {children}
+    </div>
+  );
+}
