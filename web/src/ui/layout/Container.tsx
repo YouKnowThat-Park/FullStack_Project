d52c213ce0c42a834185eb51436ae57e6789740b
wooseok <youkn0wthat@naver.com>
2025-07-06 20:33:32 +0900

    feat: add inner page Container component

--- web/src/ui/layout/Container.tsx
+++ web/src/ui/layout/Container.tsx
@@ -1,6 +1,6 @@
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="px-6 md:px-10 lg:px-[120px] max-w-screen-xl mx-auto w-full">
+    <div className="px-6 md:px-10 lg:px-[240px] max-w-screen-xl mx-auto w-full">
       {children}
     </div>
   );
