1c2115b4ee95911313c7f8dad40947a6883526ee
wooseok <youkn0wthat@naver.com>
2025-07-07 07:49:47 +0900

    feat: add AuthInputField component with forwardRef support

---
+++ web/src/app/(auth)/_components/AuthInputField.tsx
@@ -0,0 +1,29 @@
+import { forwardRef } from "react";
+
+export interface AuthInputField {
+  label: string;
+  type: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+// React Hook From으로 넘겨주기 위해선 forwardRef가 꼭 필요
+export const AuthInputField = forwardRef<HTMLInputElement, AuthInputField>(
+  ({ label, type, value, onChange }, ref) => {
+    return (
+      <div>
+        <label>{label}</label>
+        <input
+          ref={ref}
+          type={type}
+          value={value ?? ""}
+          onChange={onChange}
+          className="border"
+        />
+      </div>
+    );
+  }
+);
+
+// React는 forwardRef()로 감싸진 컴포넌트의 정보를 잃음, 그렇기에 displayName으로 명확하게 지정해줌
+AuthInputField.displayName = "AuthInputField";
