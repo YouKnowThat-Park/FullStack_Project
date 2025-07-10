16b25cfcb588ebde8d86e7bd775e14a17a6b348f
wooseok <youkn0wthat@naver.com>
2025-07-11 02:44:16 +0900

    implement API route and hook for user update (UI not connected yet)

--- web/src/app/mypage/page.tsx
+++ web/src/app/mypage/page.tsx
@@ -1,8 +1,16 @@
 "use client";
 import { useUser } from "@/hooks/useUser";
+import { AuthInputField } from "../(auth)/_components/AuthInputField";
+import { useForm } from "react-hook-form";
 
 export default function page() {
+  const { register } = useForm();
   const { data, isLoading, error } = useUser();
+
+  const onSubmit = async () => {
+    try {
+    } catch {}
+  };
 
   if (isLoading) return <div>로딩 중...</div>;
   if (error) return <div>에러 발생: {(error as Error).message}</div>;
@@ -11,6 +19,19 @@ export default function page() {
     <div>
       <h1>마이페이지</h1>
       <p>이름: {data.name}</p>
+      <AuthInputField label="이름 변경" type="text" {...register("name")} />
+      <AuthInputField
+        label="기존 비밀번호"
+        type="password"
+        {...register("old_password")}
+      />
+      <AuthInputField
+        label="기존 비밀번호"
+        type="password"
+        {...register("new_password")}
+      />
+
+      <button type="submit"></button>
     </div>
   );
 }
