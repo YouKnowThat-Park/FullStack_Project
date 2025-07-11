ccbb655158ca3ba719be26cac2e2c068224e6e8a
wooseok <youkn0wthat@naver.com>
2025-07-12 00:57:29 +0900

    🍻 feat: refresh user info after update

--- web/src/hooks/userUpdate.ts
+++ web/src/hooks/userUpdate.ts
@@ -1,11 +1,13 @@
 import { UpdateUser } from "@/type/UpdateUser";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 // PATCH를 사용한 이유는 일부 필드만 수정할꺼라서
 // PUT은 전체 필드를 덮어쓰기 때문에 내정보 수정에서 불필요
 // GET(useQuery)을 제외한 모든 PATCH, POST, PUT, DELETE는 useMutation 사용
 
 export const useUpdate = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: async (data: UpdateUser) => {
       const res = await fetch("/api/user/change-info", {
@@ -24,6 +26,9 @@ export const useUpdate = () => {
       }
 
       return result;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
     },
   });
 };
