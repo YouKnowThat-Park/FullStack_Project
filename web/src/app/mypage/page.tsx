4208995afa1e7ea1fb0d7367872616d928d997ec
wooseok <youkn0wthat@naver.com>
2025-07-12 00:36:08 +0900

    feat: connect useMutation for updating user info in page

--- web/src/app/mypage/page.tsx
+++ web/src/app/mypage/page.tsx
@@ -2,21 +2,30 @@
 import { useUser } from "@/hooks/useUser";
 import { AuthInputField } from "../(auth)/_components/AuthInputField";
 import { useForm } from "react-hook-form";
+import { useUpdate } from "@/hooks/userUpdate";
+import { UpdateUser } from "@/type/UpdateUser";
 
 export default function page() {
-  const { register } = useForm();
+  // React Hook Form 사용
+  const { register, handleSubmit } = useForm<UpdateUser>();
+
+  // 사용자 데이터 GET
   const { data, isLoading, error } = useUser();
 
-  const onSubmit = async () => {
-    try {
-    } catch {}
+  // 사용자 데이터 PATCH
+  const { mutate } = useUpdate();
+
+  const onSubmit = (formData: UpdateUser) => {
+    mutate(formData);
   };
 
   if (isLoading) return <div>로딩 중...</div>;
   if (error) return <div>에러 발생: {(error as Error).message}</div>;
   if (!data) return null;
+
+  // AuthInputField 재사용 하기
   return (
-    <div>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h1>마이페이지</h1>
       <p>이름: {data.name}</p>
       <AuthInputField label="이름 변경" type="text" {...register("name")} />
@@ -26,12 +35,11 @@ export default function page() {
         {...register("old_password")}
       />
       <AuthInputField
-        label="기존 비밀번호"
+        label="새로운 비밀번호"
         type="password"
         {...register("new_password")}
       />
-
-      <button type="submit"></button>
-    </div>
+      <button type="submit">버튼</button>
+    </form>
   );
 }
