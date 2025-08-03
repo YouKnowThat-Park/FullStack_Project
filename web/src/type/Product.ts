e02a9827a0ce256b9a92d2f64913e0ab982ecdc7
wooseok <youkn0wthat@naver.com>
2025-08-04 00:49:18 +0900

    🍻 feat: add Category and AdminProduct types

--- web/src/type/Product.ts
+++ web/src/type/Product.ts
@@ -1,8 +1,23 @@
 export interface ProductImage {
   image: string;
 }
-export interface Product {
+export interface PublicProduct {
   name: string;
   price: number;
   images: ProductImage[];
+}
+
+export interface AdminProduct {
+  name: string;
+  description: string;
+  price: number;
+  discount_price: number;
+  stock: number;
+  sold_count: number;
+  rating: number;
+  category: string;
+  is_active: boolean;
+  created_at: string;
+  updated_at: string;
+  admin_user: string;
 }
