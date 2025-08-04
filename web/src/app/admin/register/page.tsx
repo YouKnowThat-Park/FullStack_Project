6963a2b028cc2c164ae68cb951096dd846d3edb1
wooseok <youkn0wthat@naver.com>
2025-08-04 21:05:51 +0900

    feat: enhance product list UI

--- web/src/app/admin/register/page.tsx
+++ web/src/app/admin/register/page.tsx
@@ -9,20 +9,29 @@ export default function AdminProductsPage() {
   return (
     <div>
       {data?.map((product: AdminProduct) => (
-        <div key={product.id}>
-          <p>{product.name}</p>
-          <p>{product.category?.name}</p>
-          <p>{product.rating}</p>
-          <p>{product.stock}</p>
-          <p>{product.sold_count}</p>
-          <p>{product.discount_price}</p>
-          <p>{product.updated_at}</p>
-          <p>{product.admin_user}</p>
-          <p>{product.created_at}</p>
-          <p>{product.price}</p>
-          <p>{product.is_active}</p>
-          <p>{product.description}</p>
-        </div>
+        <ul key={product.id} className="border w-fit p-2 mt-2">
+          <li>
+            <div className="bg-gray-300">
+              <div className="flex">
+                <p>상품 정보: {product.name}</p>
+                <p className="ml-10 text-sm mt-1">{product.category?.name}</p>
+              </div>
+              <div className="flex justify-between p-5">
+                <p>{Number(product.price ?? 0).toLocaleString("ko-KR")} ₩</p>
+                <p>⭐: {product.rating}</p>
+                <p>재고 수량: {product.stock}</p>
+                <p>판매된 수량: {product.sold_count}</p>
+              </div>
+            </div>
+            <p>할인: {product.discount_price}</p>
+            <p>수정 날짜: {product.updated_at}</p>
+            <p>등록 관리자: {product.admin_user}</p>
+            <p>게시 날짜: {product.created_at}</p>
+            <p>상태: {product.is_active}</p>
+            <p>제품 내용: {product.description}</p>
+            <button className="border rounded-lg">수정</button>
+          </li>
+        </ul>
       ))}
     </div>
   );
