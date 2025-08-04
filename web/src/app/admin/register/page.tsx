b4fc71e61dceb331bc02a894173a4693cc3391fe
wooseok <youkn0wthat@naver.com>
2025-08-04 17:41:10 +0900

    feat: create admin products list page

--- web/src/app/admin/register/page.tsx
+++ web/src/app/admin/register/page.tsx
@@ -1,5 +1,29 @@
 "use client";
 
+import { useAdminProduct } from "@/hooks/useAdminProduct";
+import { AdminProduct } from "@/type/Product";
+
 export default function AdminProductsPage() {
-  return <div></div>;
+  const { data } = useAdminProduct();
+
+  return (
+    <div>
+      {data?.map((product: AdminProduct) => (
+        <div key={product.id}>
+          <p>{product.name}</p>
+          <p>{product.category?.name}</p>
+          <p>{product.rating}</p>
+          <p>{product.stock}</p>
+          <p>{product.sold_count}</p>
+          <p>{product.discount_price}</p>
+          <p>{product.updated_at}</p>
+          <p>{product.admin_user}</p>
+          <p>{product.created_at}</p>
+          <p>{product.price}</p>
+          <p>{product.is_active}</p>
+          <p>{product.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
