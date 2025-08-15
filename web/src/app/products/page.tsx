c847fc2a84ff4faf073a19ffd0ca37244bc45d6e
wooseok <youkn0wthat@naver.com>
2025-08-16 06:12:10 +0900

    🍻 style: Update Product design

--- web/src/app/products/page.tsx
+++ web/src/app/products/page.tsx
@@ -7,17 +7,24 @@ export default function ProductsPage() {
   const { data } = usePublicProductList();
 
   return (
-    <div>
+    <div className="grid grid-cols-4 gap-4 p-1">
       {data?.map((product: PublicProduct) => (
-        <div key={product.name}>
-          <p>{product.name}</p>
-          <p>{product.price}</p>
-          <Image
-            src={product.images[0]?.image || "/placeholder.png"}
-            alt={product.name}
-            width={100}
-            height={100}
-          />
+        <div key={product.name} className="border">
+          <div>
+            <p>{product.name}</p>
+            <p>가격: {Number(product.price).toLocaleString("ko-KR")}원</p>
+            <Image
+              src={product.images[0]?.image || "/placeholder.png"}
+              alt={product.name}
+              width={100}
+              height={100}
+            />
+            <div className="flex">
+              <span className="ml-auto text-sm border-b border-b-black">
+                Click
+              </span>
+            </div>
+          </div>
         </div>
       ))}
     </div>
