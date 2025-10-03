8571e0a6040faaa9953027122dee41e59c323ed1
wooseok <youkn0wthat@naver.com>
2025-10-04 04:09:28 +0900

    🍻 refactor : API를 RESTful API에 맞게 수정 (네트워크 응답 코드 사용)

--- web/src/hooks/product/usePublicProductList.ts
+++ web/src/hooks/product/usePublicProductList.ts
@@ -1,9 +1,9 @@
-import { fetchPublicProductList } from "@/lib/api/product/products-public";
+import { fetchPublicProduct } from "@/lib/api/product/products-public";
 import { useQuery } from "@tanstack/react-query";
 
 export function usePublicProductList() {
   return useQuery({
     queryKey: ["products"],
-    queryFn: fetchPublicProductList,
+    queryFn: fetchPublicProduct,
   });
 }
--- web/src/lib/api/product/products-public.ts
+++ web/src/lib/api/product/products-public.ts
@@ -1,10 +1,30 @@
-export const fetchPublicProductList = async () => {
+import { Category } from "@/type/product/Category";
+import { ProductImage } from "@/type/product/Product";
+
+export interface PublicProductType {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  discount_price: number | null;
+  sold_count: number;
+  rating: number;
+  category: Category | null;
+  images: ProductImage[];
+}
+
+export const fetchPublicProduct = async () => {
   const res = await fetch("http://localhost:8000/api/products/", {
     method: "GET",
   });
 
   if (!res.ok) {
-    throw new Error("상품 정보를 불러오지 못했습니다.");
+    const messages: Record<number, string> = {
+      400: "잘못된 요청 입니다.",
+      404: "요청한 URL을 찾을 수 없습니다.",
+      500: "서버 오류 입니다.",
+    };
+    throw new Error(messages[res.status] ?? "알 수 없는 오류");
   }
 
   return res.json();
